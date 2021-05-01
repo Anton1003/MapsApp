582115e9428f1b3ca6bb11cc12d8a7614bdb9291
Anton <antonilyin6@gmail.com>
2021-05-02 01:00:14 +0500

    start

--- MapsAPP/MapsAPP/Main/MainController.swift
+++ MapsAPP/MapsAPP/Main/MainController.swift
@@ -23,4 +23,34 @@ final class MainController: UIViewController {
         UserDefaults.standard.set(false, forKey: "isLogin")
         router.toLaunch()
     }
+    @IBAction func takePicture(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {return}
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .camera
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+}
+
+extension MainController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = extractImage(from: info)
+        print(image ?? UIImage())
+        picker.dismiss(animated: true)
+    }
+    
+    private func extractImage(from info: [UIImagePickerController.InfoKey: Any]) -> UIImage? {
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            return image
+        } else if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            return image
+        } else {
+            return nil
+        }
+    }
 }
